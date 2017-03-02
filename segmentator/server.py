e586a88a9ad1de8e8886f9e4798e7ff125070622
Alexander Sologub <alexsologoub@gmail.com>
2017-03-02 22:25:51 +0300

    Enable CORS

--- segmentator/server.py
+++ segmentator/server.py
@@ -4,8 +4,10 @@ import json
 from flask import Flask
 from flask import request
 from flask import jsonify
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/', methods=['GET'])
 def healthcheck():
